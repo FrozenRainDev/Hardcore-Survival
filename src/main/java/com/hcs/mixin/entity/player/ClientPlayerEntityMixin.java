c18fed85096ca97bf39a14552121bcc704aa6ff0
FrozenRainDev <1095139428@qq.com>
2023-06-07 23:19:05 +0800

    Added effects for insanity

---
+++ src/main/java/com/hcs/mixin/entity/player/ClientPlayerEntityMixin.java
@@ -0,0 +1,9 @@
+package com.hcs.mixin.entity.player;
+
+import net.minecraft.client.network.ClientPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ClientPlayerEntity.class)
+public class ClientPlayerEntityMixin {
+
+}
