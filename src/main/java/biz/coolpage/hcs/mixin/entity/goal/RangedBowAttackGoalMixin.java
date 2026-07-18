8e790226ef6d4c99189dcdde6459e3e299d6d0d7
FrozenRainDev <raindev@163.com>
2026-07-18 23:39:14 +0800

    0.19.2 misc

--- src/main/java/biz/coolpage/hcs/mixin/entity/goal/RangedBowAttackGoalMixin.java
+++ src/main/java/biz/coolpage/hcs/mixin/entity/goal/RangedBowAttackGoalMixin.java
@@ -3,6 +3,7 @@ package biz.coolpage.hcs.mixin.entity.goal;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.goal.RangedBowAttackGoal;
+import net.minecraft.world.entity.monster.RangedAttackMob;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -11,12 +12,11 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(RangedBowAttackGoal.class)
-public abstract class RangedBowAttackGoalMixin {
-    @Shadow @Final private Mob mob;
+public abstract class RangedBowAttackGoalMixin<T extends Mob & RangedAttackMob> {
+    @Shadow @Final private T mob;
     @Shadow @Final private float attackRadiusSqr;
 
     // Inject right before the vanilla code checks if the mob is using the item.
-    // Changed the target owner from LivingEntity to Mob due to Java type erasure in bytecode.
     @Inject(
             method = "tick",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Mob;isUsingItem()Z"),
@@ -26,6 +26,7 @@ public abstract class RangedBowAttackGoalMixin {
         LivingEntity target = this.mob.getTarget();
         if (target != null) {
             double distanceSqr = this.mob.distanceToSqr(target.getX(), target.getY(), target.getZ());
+
             // If the distance is greater than the attack radius (far away)
             if (distanceSqr > (double) this.attackRadiusSqr) {
                 // 1. Stop using the bow if they were drawing it out of range (prevents walking while drawing)
